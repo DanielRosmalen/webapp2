0cafa8703655ec7f50b1a1c021c4daf63f81ec42
Adani21 <1221315@student.roc-nijmegen.nl>
2026-05-26 12:26:41 +0200

    eerste commit

---
+++ app/index.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="gevoel">
+        <div>
+            <div>   <p>- soorten reizen</p>
+                <h1>Elke reis begint</h1>
+                <h1>met</h1>
+                <h1>een gevoel</h1>
+            </div>
+            <div><p>Vier richtingen waarmee onze reizigers vaak beginnen. Wij ontwerpen vanaf hier verder, in detail, met u.</p></div>
+        </div>
+        <div class="grote-blok">
+            <div class="blok"><p>01</p>
+            <h1>Strand & Eilanden</h1>
+                <p>Privé-stranden, overwater villa's,
+                    en kustregio's waar de tijd
+                    langzaam draait. </p>
+                <p>verken </p>
+            </div>
+            <div class="blok"><p>01</p>
+                <h1>Strand & Eilanden</h1>
+                <p>Privé-stranden, overwater villa's,
+                    en kustregio's waar de tijd
+                    langzaam draait. </p>
+                <p>verken </p>
+            </div>
+            <div class="blok"><p>01</p>
+                <h1>Strand & Eilanden</h1>
+                <p>Privé-stranden, overwater villa's,
+                    en kustregio's waar de tijd
+                    langzaam draait. </p>
+                <p>verken </p>
+            </div>
+            <div class="blok"><p>01</p>
+                <h1>Strand & Eilanden</h1>
+                <p>Privé-stranden, overwater villa's,
+                    en kustregio's waar de tijd
+                    langzaam draait. </p>
+                <p>verken </p>
+            </div>
+        </div>
+
+
+
+    </section>
+
+    
+</body>
+</html>
