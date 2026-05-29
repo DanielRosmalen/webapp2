f54ba8119e12b4a0e738b9cc81402bc1a2f20bd3
Adani21 <1221315@student.roc-nijmegen.nl>
2026-05-29 03:40:09 +0200

    footer begin

---
+++ app/footer.php
@@ -0,0 +1,25 @@
+﻿<?php
+?>
+html<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <footer>
+        <div class="footer-container">
+        <div class="blok1"></div>
+        <div class="blok2"></div>
+        <div class="blok3"></div>
+            <div class="blok4"></div>
+            </div>>
+    </footer>
+</body>
+</html>
+
+
