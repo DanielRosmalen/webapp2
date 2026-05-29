7ee029f5f8e01fcc36ed24ee280be2952ac0be0c
Adani21 <1221315@student.roc-nijmegen.nl>
2026-05-29 16:37:08 +0200

    footer wat meer afgemaakt nog niet alles af maar ben op de helft

--- app/footer.php
+++ app/footer.php
@@ -1,6 +1,6 @@
 ﻿<?php
 ?>
-html<!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,16 +8,39 @@ html<!doctype html>
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/footer.css?v=<?php echo time(); ?>">
 </head>
 <body>
 
     <footer>
         <div class="footer-container">
-        <div class="blok1"></div>
-        <div class="blok2"></div>
+        <div class="blok1">
+            <div class="logo"><img src="assets/img/tegna_travels_logo.png" alt="tegna logo" class=tegna-img></div>
+            <div class="korte-info">vlieg nu met het best bookingorganisatie tegna travels</div>
+            <div class="locatie">
+                <p class="locatie-titel">studio</p>
+                <p>adressregel 1</p>
+                <p>postcode plaats</p>
+            </div>
+            <div class="contact-info">
+                <p class="contact-titel">bereikbaar</p>
+                <p>telefoonnummer</p>
+                <p>emailadress</p>
+            </div>
+        </div>
+        <div class="blok2">
+            <p class="reizen-titel">Reizen</p>
+            <div class="wrap-links">
+                <p>bestemmingen</p>
+                <p>prive villa's</p>
+                <p>cruises</p>
+                <p>op maat</p>
+                <p>last-minute</p>
+            </div>
+        </div>
         <div class="blok3"></div>
             <div class="blok4"></div>
-            </div>>
+            </div>
     </footer>
 </body>
 </html>
