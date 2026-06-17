68bf9ec5b93cd6bd12cca56ad68c626347440793
Adani21 <1221315@student.roc-nijmegen.nl>
2026-06-17 15:09:40 +0200

    opnieuw begonnen met admin. side panel af alleen nog dropdown als ik hem toevoeg

--- app/admin.php
+++ app/admin.php
@@ -1,77 +1,37 @@
-<?php
-include "./acties/logcontrole.php"
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/admin.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="assets/css/admin.css">
 </head>
-
 <body>
-    <section class="full-container">
-        <header>
-            <div class="side-panel">
-                <div class="side-panel-top">
-                    <div class="side-titel-box">
-                        <h1 class="side-titel">Tegna Travels</h1>
-                    </div>
-                    <div class="side-menu-box">
-                        <nav class="side-menu">
-                            <p>Overzicht</p>
-                            <p>products</p>
-                            <p>bestelingen</p>
-                        </nav>
-                    </div>
+    <header>
+    <div class="side-panel">
+        <div class="side-panel-top">
+            <div class="side-titel-box">
+                <h1 class="side-titel">Tegna Travels</h1>
+                <p class="side-subtitel">Beheerpaneel</p>
+            </div>
+            <div class="side-menu-box">
+                <nav class="side-menu">
+                    <p class="side-menu-item actief">Overzicht</p>
+                    <p class="side-menu-item">Producten</p>
+                    <p class="side-menu-item">Bestellingen</p>
+                </nav>
+            </div>
+        </div>
+        <div class="side-panel-bottom">
+            <div class="admin-logout-container">
+                <div class="user-img">
+                    <img src="assets/img/user-pictogram.png" alt="user">
                 </div>
-                <div class="side-panel-bottom">
-                    <div class="admin-logout-container">
-                        <div class="user-img">
-                            <img src="assets/img/user-pictogram.png" alt="user">
-                        </div>
-                        <div class="dropdown">admin
-                            <div class="dropdown-content"><form method="post" action="../acties/logout.php">
-                            <button type="submit" name="loguit">Uitloggen</button>
-                        </form></div>
-                        </div>
-                    </div>
+                <div class="dropdown">admin
                 </div>
             </div>
-        </header>
-        <main>
-            <div class="welcome-container">
-                <div class="img-container"><img src="assets/img/tropisch.jpg" alt="tropisch" class="welcome-img">
-                    <h1>Welkom, admin!</h1>
-                    <p>Hier vind je een overzicht van de belangrijkste functies.</p>
-                </div>
-            </div>
-            <div class="overzicht-container">
-                <div class="overzicht-box">
-                    <h2>Boekingen Overzicht</h2>
-                    <p>Dit is het overzicht van alle boekingen.</p>
-                </div>
-                <div class="overzicht-box">
-                    <h2>Producten Overzicht</h2>
-                    <p>Dit is het overzicht van alle producten.</p>
-                </div>
-                <div class="overzicht-box">
-                    <h2>Klanten Overzicht</h2>
-                    <p>Dit is het overzicht van alle klanten.</p>
-                </div>
-            </div>
-            <div class="inkomende-vluchten-container">
-                <h2>Inkomende Vluchten <span></span></h2>
-                <p>Dit is het overzicht van inkomende vluchten.</p>
-            </div>
-        </main>
-    </section>
-
+        </div>
+    </div>
+</header>
 </body>
-
 </html>
